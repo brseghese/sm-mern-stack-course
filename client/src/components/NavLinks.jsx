c69b1fc2972201d09caab49fd3bf3615810607fc
brseghese <brseghese@gmail.com>
2023-11-18 16:40:10 -0300

    client - navlinks component

---
+++ client/src/components/NavLinks.jsx
@@ -0,0 +1,30 @@
+import { useDashboardContext } from '../pages/DashboardLayout';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
+
+const NavLinks = () => {
+  const { toggleSidebar } = useDashboardContext();
+
+  return (
+    <div className='nav-links'>
+      {links.map((link) => {
+        const { text, path, icon } = link;
+
+        return (
+          <NavLink
+            to={path}
+            key={text}
+            className='nav-link'
+            onClick={toggleSidebar}
+            end
+          >
+            <span className='icon'>{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavLinks;
--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -2,8 +2,7 @@ import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { useDashboardContext } from '../pages/DashboardLayout';
 import Logo from './Logo';
-import links from '../utils/links';
-import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
   const { showSidebar, toggleSidebar } = useDashboardContext();
@@ -22,23 +21,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className='nav-links'>
-            {links.map((link) => {
-              const { text, path, icon } = link;
-              return (
-                <NavLink
-                  to={path}
-                  key={text}
-                  className='nav-link'
-                  onClick={toggleSidebar}
-                  end
-                >
-                  <span className='icon'>{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks />
         </div>
       </div>
     </Wrapper>
