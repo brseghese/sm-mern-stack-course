b2b252134449289034e80c24eb83768628b8ad1b
brseghese <brseghese@gmail.com>
2023-11-18 15:12:33 -0300

    client - navbar structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import Wrapper from '../assets/wrappers/Navbar';
+import { FaBars } from 'react-icons/fa';
+import Logo from './Logo';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  const { toggleSidebar } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <nav className='nav-center'>
+        <button type='button' className='toggle-btn' onClick={toggleSidebar}>
+          <FaBars />
+        </button>
+        <div>
+          <Logo />
+          <h4 className='logo-text'>Painel de Controle</h4>
+        </div>
+        <div className='btn-container'>toggle/logout</div>
+      </nav>
+    </Wrapper>
+  );
 };
 
 export default Navbar;
