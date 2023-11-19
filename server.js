742117c34746f54d2dd93339acc1ec889adf914e
brseghese <brseghese@gmail.com>
2023-11-19 16:42:20 -0300

    server - get all jobs

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+let jobs = [
+  { id: nanoid(), company: 'apple', position: 'front-end' },
+  { id: nanoid(), company: 'google', position: 'back-end' },
+];
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -17,6 +23,10 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   console.log(req);
   res.json({ message: 'data received', data: req.body });
+});
+
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5000;
