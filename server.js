fe4fe3ab20ee7b6708fd20badbe1e8ac1cb721da
brseghese <brseghese@gmail.com>
2023-11-19 18:06:09 -0300

    server - edit job

--- server.js
+++ server.js
@@ -52,6 +52,22 @@ app.get('/api/v1/jobs/:id', (req, res) => {
   res.status(200).json({ job });
 });
 
+// Edit Job
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: 'please provide company and position' });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: 'job modified', job });
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
