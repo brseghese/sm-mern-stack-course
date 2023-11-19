0fc2596fdcc7fa00c30d759a4c75887e63091f4f
brseghese <brseghese@gmail.com>
2023-11-19 04:48:16 -0300

    client - dark theme logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,14 @@ import {
   Admin,
 } from './pages';
 
+export const checkDefaultTheme = () => {
+  const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+  document.body.classList.toggle('dark-theme', isDarkTheme);
+  return isDarkTheme;
+};
+
+checkDefaultTheme();
+
 const router = createBrowserRouter([
   {
     path: '/',
--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { SmallSidebar, BigSidebar, Navbar } from '../components';
 import { createContext, useContext, useState } from 'react';
+import { checkDefaultTheme } from '../App';
 
 const DashboardContext = createContext();
 
@@ -9,10 +10,13 @@ const DashboardLayout = () => {
   // temp
   const user = { name: 'Bruno' };
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
 
   const toggleDarkTheme = () => {
-    console.log('toggle dark theme');
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
   };
 
   const toggleSidebar = () => {
