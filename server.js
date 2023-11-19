815c8833aecc45240f1c6ac0f7c0355e732afe84
brseghese <brseghese@gmail.com>
2023-11-19 18:15:08 -0300

    server - delete job

--- server.js
+++ server.js
@@ -68,6 +68,18 @@ app.patch('/api/v1/jobs/:id', (req, res) => {
   res.status(200).json({ msg: 'job modified', job });
 });
 
+// Delete Job
+app.delete('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+  res.status(200).json({ msg: 'job deleted' });
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
