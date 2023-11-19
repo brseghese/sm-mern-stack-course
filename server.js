a1b937789821de1897f8b00b796163264217981b
brseghese <brseghese@gmail.com>
2023-11-19 17:53:01 -0300

    server - get single job

--- server.js
+++ server.js
@@ -34,13 +34,21 @@ app.get('/api/v1/jobs', (req, res) => {
 app.post('/api/v1/jobs', (req, res) => {
   const { company, position } = req.body;
   if (!company || !position) {
-    return res
-      .status(400)
-      .json({ message: 'please provide company and position' });
+    return res.status(400).json({ msg: 'please provide company and position' });
   }
   const id = nanoid(10);
   const job = { id, company, position };
   jobs.push(job);
+  res.status(201).json({ job });
+});
+
+// Get Single Job
+app.get('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
   res.status(200).json({ job });
 });
 
