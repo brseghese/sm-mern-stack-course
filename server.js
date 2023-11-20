789eccce4a2057322f2e70bd36a516d0d136bedb
brseghese <brseghese@gmail.com>
2023-11-20 18:22:36 -0300

    server - connect mongo db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 // Routers
 import jobRouter from './routes/jobRouter.js';
@@ -35,6 +36,12 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}...`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}...`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
