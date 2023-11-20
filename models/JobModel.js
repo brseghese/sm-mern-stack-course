fa625f4e99a2eb1e54637f76cf18ea01a4c9ccf1
brseghese <brseghese@gmail.com>
2023-11-20 18:36:48 -0300

    server - job model

---
+++ models/JobModel.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: String,
+    position: String,
+    jobStatus: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    jobType: {
+      type: String,
+      enum: ['full-time', 'part-time', 'internship'],
+      default: 'full-time',
+    },
+    jobLocation: {
+      type: String,
+      default: 'my city',
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Job', JobSchema);
