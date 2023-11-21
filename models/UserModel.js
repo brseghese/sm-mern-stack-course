d5fd61cc3784353b582adebc7e620ec9a3edf3dc
brseghese <brseghese@gmail.com>
2023-11-21 18:04:35 -0300

    server - user model

---
+++ models/UserModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  lastName: {
+    type: String,
+    default: 'lastName',
+  },
+  location: {
+    type: String,
+    default: 'my city',
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+export default mongoose.model('User', UserSchema);
