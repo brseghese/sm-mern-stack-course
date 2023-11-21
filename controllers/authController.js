5ed6f9e07c9af37f38233b56e4477acb95d5019a
brseghese <brseghese@gmail.com>
2023-11-21 18:35:51 -0300

    server - create user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,9 @@
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/UserModel.js';
+
 export const register = async (req, res) => {
-  res.send('register');
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 export const login = async (req, res) => {
