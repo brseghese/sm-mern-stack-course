bd9e8014780370d70531a2d42c9b5aed3c1721bb
brseghese <brseghese@gmail.com>
2023-11-21 05:10:15 -0300

    server - express validator setup

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from 'express';
 const app = express();
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import { body, validationResult } from 'express-validator';
 
 // Routers
 import jobRouter from './routes/jobRouter.js';
@@ -22,10 +23,28 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.post('/', (req, res) => {
-  console.log(req);
-  res.json({ message: 'data received', data: req.body });
-});
+app.post(
+  '/api/v1/test',
+  [
+    body('name')
+      .notEmpty()
+      .withMessage('name is required')
+      .isLength({ min: 50 })
+      .withMessage('name must be at least 50'),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessage = errors.array().map((error) => error.msg);
+      return res.status(400).json({ errors: errorMessage });
+    }
+    next();
+  },
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `hello ${name}` });
+  }
+);
 
 app.use('/api/v1/jobs', jobRouter);
 
