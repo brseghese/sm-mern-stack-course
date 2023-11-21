4482c405a21b3aabb689c6f4579729785ce6a36c
brseghese <brseghese@gmail.com>
2023-11-21 04:24:25 -0300

    server - more custom errors

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -7,3 +7,27 @@ export class NotFoundError extends Error {
     this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
+
+export class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'BadRequestError';
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+export class UnauthenticatedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'UnauthenticatedError';
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+export class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'UnauthorizedError';
+    this.statusCode = StatusCodes.FORBIDDEN;
+  }
+}
