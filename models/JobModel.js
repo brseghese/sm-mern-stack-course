fdba0d8f41eb0efe4dd4d79af4e2e56fa9bfdb51
brseghese <brseghese@gmail.com>
2023-11-21 05:46:02 -0300

    server - constants

--- models/JobModel.js
+++ models/JobModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { JOB_STATUS, JOB_TYPE } from '../utils/constants';
 
 const JobSchema = new mongoose.Schema(
   {
@@ -6,13 +7,13 @@ const JobSchema = new mongoose.Schema(
     position: String,
     jobStatus: {
       type: String,
-      enum: ['interview', 'declined', 'pending'],
-      default: 'pending',
+      enum: Object.values(JOB_STATUS),
+      default: JOB_STATUS.PENDING,
     },
     jobType: {
       type: String,
-      enum: ['full-time', 'part-time', 'internship'],
-      default: 'full-time',
+      enum: Object.values(JOB_TYPE),
+      default: JOB_TYPE.FULL_TIME,
     },
     jobLocation: {
       type: String,
