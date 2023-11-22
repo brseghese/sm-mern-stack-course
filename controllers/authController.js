4425c568aedcc521974aa6577ccaa05e5702e687
brseghese <brseghese@gmail.com>
2023-11-22 04:22:36 -0300

    server - admin user

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 
 export const register = async (req, res) => {
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
