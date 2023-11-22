0318c3e93f63757d2b052038e3ae480fb9cecfdc
brseghese <brseghese@gmail.com>
2023-11-22 06:31:40 -0300

    server - http only cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,9 +17,19 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
+
   const isValidUser =
     user && (await comparePassword(req.body.password, user.password));
   if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
+
   const token = createJWT({ userId: user._id, role: user.role });
-  res.json({ token });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
+
+  res.status(StatusCodes.OK).json({ msg: 'user logged in' });
 };
