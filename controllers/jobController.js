de4d357921c51210f00b93f0e969fd85d50a9f70
brseghese <brseghese@gmail.com>
2023-11-22 17:02:03 -0300

    server - add user to job routes

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,12 +2,12 @@ import Job from '../models/JobModel.js';
 import { StatusCodes } from 'http-status-codes';
 
 export const getAllJobs = async (req, res) => {
-  console.log(req.user);
-  const jobs = await Job.find({});
+  const jobs = await Job.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
