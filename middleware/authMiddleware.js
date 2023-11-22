ca22b53ce5a66a8bf1894d0f9efff6740e074832
brseghese <brseghese@gmail.com>
2023-11-22 16:08:54 -0300

    server - verify cookie

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,7 @@
+import { UnauthenticatedError } from '../errors/customErrors.js';
+
 export const authenticateUser = async (req, res, next) => {
-  console.log('auth middleware');
+  const { token } = req.cookies;
+  if (!token) throw new UnauthenticatedError('authentication invalid');
   next();
 };
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from 'express';
 const app = express();
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 
 // Routers
 import jobRouter from './routes/jobRouter.js';
@@ -18,6 +19,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get('/', (req, res) => {
