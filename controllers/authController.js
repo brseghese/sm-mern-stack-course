7bb8b3ddbdffe204ef07f20b821c9d7b17ae5f82
brseghese <brseghese@gmail.com>
2023-11-22 05:54:43 -0300

    server - jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
 import { UnauthenticatedError } from '../errors/customErrors.js';
+import { createJWT } from '../utils/tokenUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -19,5 +20,6 @@ export const login = async (req, res) => {
   const isValidUser =
     user && (await comparePassword(req.body.password, user.password));
   if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
-  res.send('login');
+  const token = createJWT({ userId: user._id, role: user.role });
+  res.json({ token });
 };
---
+++ utils/tokenUtils.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+
+export const createJWT = (payload) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  return token;
+};
