721cb0bcd1c6cc280d4ee3e95b5bef0ade2b48f3
brseghese <brseghese@gmail.com>
2023-11-22 05:20:49 -0300

    server - login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
-import { hashPassword } from '../utils/passwordUtils.js';
+import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
+import { UnauthenticatedError } from '../errors/customErrors.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -14,5 +15,9 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
+  if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
   res.send('login');
 };
