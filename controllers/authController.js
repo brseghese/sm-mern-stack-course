15ae252270b0e7b1a9e725a169d5bc766026f3e6
brseghese <brseghese@gmail.com>
2023-11-22 04:36:58 -0300

    server - hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
 
 export const login = async (req, res) => {
