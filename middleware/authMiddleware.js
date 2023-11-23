2ebbc2960a934499b353f6f0178b4fc15e99ff07
brseghese <brseghese@gmail.com>
2023-11-23 03:26:31 -0300

    server - refactor auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 import { UnauthenticatedError } from '../errors/customErrors.js';
 import { verifyJWT } from '../utils/tokenUtils.js';
 
-export const authenticateUser = async (req, res, next) => {
+export const authenticateUser = (req, res, next) => {
   const { token } = req.cookies;
   if (!token) throw new UnauthenticatedError('authentication invalid');
 
