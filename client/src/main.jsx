1ca586f33c0a16607c8b97d3b08bc7a3afe01ae7
brseghese <brseghese@gmail.com>
2023-11-23 05:55:22 -0300

    server & client - proxy setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+fetch('/api/v1/test')
+  .then((res) => res.json())
+  .then((data) => console.log(data));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/api/v1/test', (req, res) => {
+  res.json({ msg: 'test route' });
+});
+
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
