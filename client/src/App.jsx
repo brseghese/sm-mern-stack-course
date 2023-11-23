a878dce84a7df1587c79c355556e25e1c2e978ad
brseghese <brseghese@gmail.com>
2023-11-23 14:59:31 -0300

    client - login user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import {
 } from './pages';
 
 import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -41,6 +42,7 @@ const router = createBrowserRouter([
       {
         path: 'login',
         element: <Login />,
+        action: loginAction,
       },
       {
         path: 'dashboard',
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, Form, redirect, useNavigation } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
-import { Logo, FormRow } from '../components';
+import { FormRow, Logo } from '../components';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post('/auth/login', data);
+    toast.success('Login realizado com sucesso!');
+    return redirect('/dashboard');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Login = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
     <Wrapper>
-      <form className='form'>
+      <Form method='post' className='form'>
         <Logo />
         <h4>Entrar na Conta</h4>
         <FormRow
@@ -20,8 +39,8 @@ const Login = () => {
           labelText='senha'
           defaultValue='secret123'
         />
-        <button type='submit' className='btn btn-block'>
-          Enviar
+        <button type='submit' className='btn btn-block' disabled={isSubmitting}>
+          {isSubmitting ? 'Enviando...' : 'Enviar'}
         </button>
         <p>
           Não tem conta?
@@ -29,7 +48,7 @@ const Login = () => {
             Criar uma Conta
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
