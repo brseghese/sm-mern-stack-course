ed63c47bb0d9ea36f1d9b2e93e464f1eec2343e6
brseghese <brseghese@gmail.com>
2023-11-23 06:21:00 -0300

    client - remove axios custom fetch test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import customFetch from './utils/customFetch.js';
-
-const resp = await customFetch.get('/test');
-console.log(resp);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
