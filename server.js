094dfbd1d7291db1e430a63f4057397f4c69382c
brseghese <brseghese@gmail.com>
2023-11-26 15:51:36 -0300

    server - avatar & public folder

--- server.js
+++ server.js
@@ -12,14 +12,22 @@ import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
 
+// Public
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 // Middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authenticateUser } from './middleware/authMiddleware.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(express.static(path.resolve(__dirname, './public')));
 app.use(cookieParser());
 app.use(express.json());
 
