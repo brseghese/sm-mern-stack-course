1b717247fbd86d2d6837df3ba377e46e99de8e94
brseghese <brseghese@gmail.com>
2023-11-26 05:56:41 -0300

    client - edit job jsx

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,6 +1,6 @@
 import { FormRow, FormRowSelect } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
-import { useLoaderData, useParams } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
 import { Form, useNavigation, redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -22,9 +22,56 @@ export const action = async () => {
 
 const EditJob = () => {
   const { job } = useLoaderData();
-  console.log(job);
 
-  return <h1>EditJob Page</h1>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <Wrapper>
+      <Form method='post' className='form'>
+        <h4 className='form-title'>Editar Trabalho</h4>
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='position'
+            labelText='Cargo'
+            defaultValue={job.position}
+          />
+          <FormRow
+            type='text'
+            name='company'
+            labelText='Empresa'
+            defaultValue={job.company}
+          />
+          <FormRow
+            type='text'
+            labelText='Localização'
+            name='jobLocation'
+            defaultValue={job.jobLocation}
+          />
+          <FormRowSelect
+            name='jobStatus'
+            labelText='Status'
+            defaultValue={job.jobStatus}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            name='jobType'
+            labelText='Período'
+            defaultValue={job.jobType}
+            list={Object.values(JOB_TYPE)}
+          />
+          <button
+            type='submit'
+            className='btn btn-block form-btn '
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Enviando...' : 'Enviar'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default EditJob;
