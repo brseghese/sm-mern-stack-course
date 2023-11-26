3516342a3f9df28b2b397908faace253c4ae20a0
brseghese <brseghese@gmail.com>
2023-11-26 05:17:11 -0300

    client - route params

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,6 +19,8 @@ import { action as loginAction } from './pages/Login';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
 import { action as addJobAction } from './pages/AddJob';
 import { loader as allJobsLoader } from './pages/AllJobs';
+import { loader as editJobLoader } from './pages/EditJob';
+import { action as editJobAction } from './pages/EditJob';
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -78,6 +80,8 @@ const router = createBrowserRouter([
           {
             path: 'edit-job/:id',
             element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
           },
         ],
       },
--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,4 +1,24 @@
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useLoaderData, useParams } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+export const loader = async ({ params }) => {
+  console.log(params);
+  return null;
+};
+
+export const action = async () => {
+  return null;
+};
+
 const EditJob = () => {
+  const params = useParams();
+  console.log(params);
+
   return <h1>EditJob Page</h1>;
 };
 
