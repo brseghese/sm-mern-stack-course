3c875dd0597e71a6ccd3e8b0e767422ebf6a06a7
brseghese <brseghese@gmail.com>
2023-11-29 08:19:21 -0300

    server - group by monthly applications

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -51,20 +51,36 @@ export const showStats = async (req, res) => {
     declined: stats.encerrado || 0,
   };
 
-  let monthlyApplications = [
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
     {
-      date: 'May 23',
-      count: 12,
+      $group: {
+        _id: {
+          year: { $year: '$createdAt' },
+          month: { $month: '$createdAt' },
+        },
+        count: { $sum: 1 },
+      },
     },
-    {
-      date: 'Jun 23',
-      count: 9,
-    },
-    {
-      date: 'Jul 23',
-      count: 3,
-    },
-  ];
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+
+      const date = day()
+        .month(month - 1)
+        .year(year)
+        .format('MMM YY');
+
+      return { date, count };
+    })
+    .reverse();
 
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
