52fb09fc323220e4edf5652043fff64a8d0c8d66
brseghese <brseghese@gmail.com>
2023-11-29 09:57:59 -0300

    client - stats container

--- client/src/components/StatsContainer.jsx
+++ client/src/components/StatsContainer.jsx
@@ -1,5 +1,38 @@
-const StatsContainer = () => {
-  return <h1>StatsContainer</h1>;
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from 'react-icons/fa';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import StatItem from './StatItem';
+
+const StatsContainer = ({ defaultStats }) => {
+  const stats = [
+    {
+      title: 'candidaturas pendentes',
+      count: defaultStats?.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: '#f59e0b',
+      bcg: '#fef3c7',
+    },
+    {
+      title: 'entrevistas agendadas',
+      count: defaultStats?.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: '#647acb',
+      bcg: '#e0e8f9',
+    },
+    {
+      title: 'cadidaturas encerradas',
+      count: defaultStats?.declined || 0,
+      icon: <FaBug />,
+      color: '#d66a6a',
+      bcg: '#ffeeee',
+    },
+  ];
+  return (
+    <Wrapper>
+      {stats.map((item) => {
+        return <StatItem key={item.title} {...item} />;
+      })}
+    </Wrapper>
+  );
 };
 
 export default StatsContainer;
