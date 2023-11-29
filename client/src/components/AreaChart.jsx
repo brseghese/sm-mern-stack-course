1e525696308ce899dd74bfcc3f0035ec54ab6e73
brseghese <brseghese@gmail.com>
2023-11-29 15:26:56 -0300

    client - charts

--- client/src/components/AreaChart.jsx
+++ client/src/components/AreaChart.jsx
@@ -1,5 +1,25 @@
-const AreaChart = () => {
-  return <h1>AreaChart</h1>;
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
+
+const AreaChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width='100%' height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type='monotone' dataKey='count' stroke='#bef8fd' fill='#2cb1bc' />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 };
 
-export default AreaChart;
+export default AreaChartComponent;
--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,5 +1,20 @@
-const ChartsContainer = () => {
-  return <h1>ChartsContainer</h1>;
+import { useState } from 'react';
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true);
+
+  return (
+    <Wrapper>
+      <h4>Candidaturas Mensais</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Grafico de Área' : 'Grafico de Barras'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
