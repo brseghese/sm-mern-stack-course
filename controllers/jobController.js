bd926ef7e0c200a720e1b7b09f6fcb633bc4b777
brseghese <brseghese@gmail.com>
2023-11-29 16:02:37 -0300

    server - charts last twelve months

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -63,7 +63,7 @@ export const showStats = async (req, res) => {
       },
     },
     { $sort: { '_id.year': -1, '_id.month': -1 } },
-    { $limit: 6 },
+    { $limit: 12 },
   ]);
 
   monthlyApplications = monthlyApplications
