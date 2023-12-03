b10564d6006b00d70b4771aac5b1fdd08a16d11e
brseghese <brseghese@gmail.com>
2023-12-03 06:01:45 -0300

    client - debounce search

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -9,6 +9,17 @@ const SearchContainer = () => {
   const { search, jobStatus, jobType, sort } = searchValues;
   const submit = useSubmit();
 
+  const debounce = (onChange) => {
+    let timeout;
+    return (e) => {
+      const form = e.currentTarget.form;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        onChange(form);
+      }, 2000);
+    };
+  };
+
   return (
     <Wrapper>
       <Form className='form'>
@@ -19,9 +30,9 @@ const SearchContainer = () => {
             name='search'
             labelText='empresa / cargo'
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form);
-            }}
+            onChange={debounce((form) => {
+              submit(form);
+            })}
           />
           <FormRowSelect
             labelText='status'
