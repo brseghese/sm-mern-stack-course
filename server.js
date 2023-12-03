a97fd673bf3d1b917bf7a90caf0a8ec7b5d9bfd3
brseghese <brseghese@gmail.com>
2023-12-03 17:29:13 -0300

    server - local build

--- server.js
+++ server.js
@@ -50,6 +50,10 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
+
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
 });
