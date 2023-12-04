c010fad1e35f26c59382b665fc4eb44026460a12
brseghese <brseghese@gmail.com>
2023-12-04 16:58:30 -0300

    server - security packages

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 const router = Router();
-
 import { login, logout, register } from '../controllers/authController.js';
-
 import {
   validateRegisterInput,
   validateLoginInput,
 } from '../middleware/validationMiddleware.js';
+import rateLimiter from 'express-rate-limit';
 
-router.post('/register', validateRegisterInput, register);
-router.post('/login', validateLoginInput, login);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 15,
+  message: { msg: 'IP rate limit exceeded, retry in 15 minutes.' },
+});
+
+router.post('/register', apiLimiter, validateRegisterInput, register);
+router.post('/login', apiLimiter, validateLoginInput, login);
 router.get('/logout', logout);
 
 export default router;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import cloudinary from 'cloudinary';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
 // Routers
 import jobRouter from './routes/jobRouter.js';
@@ -37,14 +39,8 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.get('/api/v1/test', (req, res) => {
-  res.json({ msg: 'test route' });
-});
+app.use(helmet());
+app.use(mongoSanitize());
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
